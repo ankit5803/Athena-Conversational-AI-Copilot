58ae0ab8e307910a4d76e48b0ede8fd0064b9df3
Sandarva Podder <sandarvapodder09032004@gmail.com>
2025-10-21 12:52:56 +0530

    fixed pre-rendering static contents

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import AIAssistantUI from "@/components/AIAssistantUI";
-// import axios from "axios";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import { useChat } from "@/components/contexts/ChatContext";
 export default function ChatPage() {
   const { fetchUserDatandsaveUser } = useChat();
-  fetchUserDatandsaveUser();
+  useEffect(() => {
+    fetchUserDatandsaveUser();
+  }, []);
   return <AIAssistantUI />;
 }
